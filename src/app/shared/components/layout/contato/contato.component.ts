a9103918e6fc1f9ba956362dee2ba7dc0e72ae8e
Breno Henrique <brenohdias123@gmail.com>
2022-04-08 22:36:58 -0300

    :sparkles: Cria uma tabela na página de contatos

--- src/app/shared/components/layout/contato/contato.component.ts
+++ src/app/shared/components/layout/contato/contato.component.ts
@@ -17,7 +17,13 @@ export class ContatoComponent implements OnInit {
   valorMaisQuatro: number = 0;
   valorMaisDez: number = 0;
 
+  contacts = [
+    {name:'Breno', idade: '19', cpf:'12345678901', email:'breno@gmail.com'},
+    {name:'Fernando', idade: '20', cpf:'12345678902', email:'fernando@gmail.com' },
+    {name:'Ivson', idade: '21', cpf:'12345678903', email:'ivson@gmail.com'},
+    {name:'Thiago', idade: '22', cpf:'12345678904', email:'thiago@gmail.com'},
 
+  ]
 
 
   @Output() mudouValor = new EventEmitter();
