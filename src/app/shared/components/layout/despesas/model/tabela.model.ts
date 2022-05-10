04b51d3e6c2ba43ab83e1ea738bf372b333f3824
Breno Henrique <brenohdias123@gmail.com>
2022-05-10 20:57:11 -0300

    :construction: Configura tabela de despesas

--- src/app/shared/components/layout/despesas/model/tabela.model.ts
+++ src/app/shared/components/layout/despesas/model/tabela.model.ts
@@ -3,8 +3,10 @@ import { TableStandard } from "src/app/shared/models/table.model";
 export const tableArr: TableStandard[] = [
   {hintName: 'Código da unidade de negócio', field: 'code', header: 'CODE', showCol: true },
   {hintName: 'Nome da unidade de negócio', field: 'type', header: 'TYPE', showCol: true },
-  {hintName: 'Município da unidade de negócio', field: 'idade', header: 'IDADE', showCol: true },
-  {hintName: 'UF da unidade de negócio', field: 'cpf', header: 'CPF', showCol: true },
-  {hintName: 'UF da unidade de negócio', field: 'email', header: 'EMAIL', showCol: true },
+  {hintName: 'Município da unidade de negócio', field: 'description', header: 'DESCRIPTION', showCol: true },
+  {hintName: 'UF da unidade de negócio', field: 'value', header: 'VALUE', showCol: true },
+  {hintName: 'UF da unidade de negócio', field: 'typePayment', header: 'TIPO PAGAMENTO', showCol: true },
+  {hintName: 'UF da unidade de negócio', field: 'localEstablishment', header: 'ESTABELECIMENTO', showCol: true },
+  {hintName: 'UF da unidade de negócio', field: 'expenseDate', header: 'DATA', showCol: true },
   {hintName: 'Operações Disponíveis', field: 'operation', header: 'OPERAÇÕES', showCol: true }
 ];
