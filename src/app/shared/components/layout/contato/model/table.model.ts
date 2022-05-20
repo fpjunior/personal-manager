e9161d4e599642789c97fea82eb0075d737344d0
Breno Henrique <brenohdias123@gmail.com>
2022-05-19 23:16:45 -0300

    :sparkles: Limita o campo de idade no dialog de cadastro de contato

--- src/app/shared/components/layout/contato/model/table.model.ts
+++ src/app/shared/components/layout/contato/model/table.model.ts
@@ -1,11 +1,11 @@
 import { TableStandard } from "src/app/shared/models/table.model";
 
 export const tableArr: TableStandard[] = [
-  // {hintName: 'Código da unidade de negócio', field: 'id', header: 'ID', showCol: true },
+  //{hintName: 'Código da unidade de negócio', field: 'id', header: 'ID', showCol: true },
   {hintName: 'Nome do contato', field: 'name', header: 'NOME', showCol: true },
   {hintName: 'Idade do contato', field: 'age', header: 'IDADE', showCol: true },
   {hintName: 'CPF do contato', field: 'cpf', header: 'CPF', showCol: true },
   {hintName: 'Telefone do contato', field: 'phone', header: 'TELEFONE', showCol: true },
-  {hintName: 'Email do contato', field: 'email', header: 'EMAIL', showCol: true },
+  {hintName: 'Email do contatogit ', field: 'email', header: 'EMAIL', showCol: true },
   {hintName: 'Operações Disponíveis', field: 'operation', header: 'OPERAÇÕES', showCol: true },
 ];
