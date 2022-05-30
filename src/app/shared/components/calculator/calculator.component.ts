4d97c976bd5c4b42c8a7bdacd49143b911649ce6
Breno Henrique <brenohdias123@gmail.com>
2022-05-30 19:36:27 -0300

    :sparkles: Conclui a calculadora

--- src/app/shared/components/calculator/calculator.component.ts
+++ src/app/shared/components/calculator/calculator.component.ts
@@ -18,6 +18,7 @@ export class CalculatorComponent implements OnInit {
   valueDisplay: any = "";
   valueNumber = 7;
   teste: any;
+  operator: string;
   @Output() onClose: EventEmitter<boolean> = new EventEmitter<boolean>();
   @Output() onShow: EventEmitter<boolean> = new EventEmitter<boolean>();
 
@@ -37,16 +38,15 @@ export class CalculatorComponent implements OnInit {
   }
 
   deleteLast(){
-    //this.teste= this.valueDisplay;
     this.valueDisplay = this.valueDisplay.substr(0, this.valueDisplay.length - 1);
     return this.valueDisplay;
     }
 
   calculate() {
-    // let result =
-    //   Number(this.valueDisplay.split("+")[0]) +
-    //   Number(this.valueDisplay.split("+")[1])
-    // this.valueDisplay = result.toString();
+    //let result =
+    //  Number(this.valueDisplay.split("+")[0]) +
+    //  Number(this.valueDisplay.split("+")[1])
+    //this.valueDisplay = result.toString();
     this.valueDisplay =  eval(this.valueDisplay);  //essa linha faz o que as linhas acima estava fazendo por conta dessa função eval
     return this.valueDisplay;
   }
@@ -61,7 +61,7 @@ export class CalculatorComponent implements OnInit {
     // {{showModalCalc}}
     // ESC key
 
-    /*switch (event.key) {
+    switch (event.key) {
       case "9":
         this.valueDisplay += "9";
         break;
@@ -95,8 +95,23 @@ export class CalculatorComponent implements OnInit {
       case "+":
         this.valueDisplay += "+";
         break;
-      case "+":
-        this.valueDisplay += "+";
+      case "-":
+        this.valueDisplay += "-";
+        break;
+      case "*":
+        this.valueDisplay += "*";
+        break;
+      case "/":
+        this.valueDisplay += "/";
+        break;
+      case "(":
+        this.valueDisplay += "(";
+        break;
+      case ")":
+        this.valueDisplay += ")";
+        break;
+      case ".":
+        this.valueDisplay += ".";
         break;
       case "Enter":
         this.calculate();
@@ -107,21 +122,6 @@ export class CalculatorComponent implements OnInit {
         break;
       default:
         break;
-    }*/
-
-    if (event.key === "Enter"|| event.key === "=") {
-      this.calculate();
-      this.deleteLast();
-    }else
-    //Esse if pega qual tecla foi digitada no teclado e mostra na tela
-    if (event.key === "9" || event.key === "8" || event.key === "7" || event.key === "6" || event.key === "5"
-    || event.key === "4" || event.key === "3" || event.key === "2" || event.key === "1" || event.key === "0"
-    || event.key === "+" || event.key === "-" || event.key === "*" || event.key === "/") {
-      this.valueDisplay += event.key;
-    }else
-    //Esse if é para apagar o ultimo ultimo item digitado ao clicar no botao de apagar do teclado
-    if(event.key === "Backspace"){
-      this.deleteLast();
     }
 
   }
