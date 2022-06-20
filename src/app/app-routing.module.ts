46907820386a52f451141e62e382a924a613d3ed
fpjuniorpx <fpjuniorpx@gmail.com>
2022-06-19 21:50:48 -0300

    :art: mudanças de layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -34,6 +34,11 @@ const routes: Routes = [
     canActivate: [AuthGuard],
     component: TiposdespesasComponent,
   },
+  {
+    path: "home",
+    canActivate: [AuthGuard],
+    component: HomeComponent,
+  },
 ];
 
 @NgModule({
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -79,6 +79,13 @@ export class HeaderComponent implements OnInit {
         this.visibleSidebar = false;
         this.route.navigate(['tiposdespesas']);
       },
+    },    {
+      label: 'Home',
+      icon: 'fa fa-address-book',
+      command: () => {
+        this.visibleSidebar = false;
+        this.route.navigate(['home']);
+      },
     },
   ]);
 }
