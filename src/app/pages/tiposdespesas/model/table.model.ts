2896b5e4fe36a51820d7aa4243b9b9a94b16fa45
ivsonramos <ivson.ramos@gmail.com>
2022-06-27 23:48:23 -0300

    :bug: Ajustar o tooltip da coluna icone para Icone da despesa

--- src/app/pages/tiposdespesas/model/table.model.ts
+++ src/app/pages/tiposdespesas/model/table.model.ts
@@ -2,7 +2,7 @@ import { TableStandard } from "src/app/shared/models/table.model";
 
 export const tableTipoDespesaModel: TableStandard[] = [
   // {hintName: 'Código do tipos despesas', field: 'code', header: 'CODE', showCol: true },
-  {hintName: 'Descrição do tipos despesas', field: 'icon', header: 'Icone', showCol: true },
+  {hintName: 'Icone da despesa', field: 'icon', header: 'Icone', showCol: true },
   {hintName: 'Tipos despesas', field: 'name', header: 'Nome', showCol: true },
   {hintName: 'Cor da despesa', field: 'cor', header: 'Cor', showCol: true },
   {hintName: 'Operações Disponíveis', field: 'operation', header: 'OPERAÇÕES', showCol: true }
