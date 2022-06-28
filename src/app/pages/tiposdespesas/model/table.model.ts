17f5031aaf50b733cc6d3237b8990a7d3844e6f2
ivsonramos <ivson.ramos@gmail.com>
2022-06-27 23:39:47 -0300

    :bug: Ajustar o tooltip da coluna cor para Cor da despesa

--- src/app/pages/tiposdespesas/model/table.model.ts
+++ src/app/pages/tiposdespesas/model/table.model.ts
@@ -4,6 +4,6 @@ export const tableTipoDespesaModel: TableStandard[] = [
   // {hintName: 'Código do tipos despesas', field: 'code', header: 'CODE', showCol: true },
   {hintName: 'Descrição do tipos despesas', field: 'icon', header: 'Icone', showCol: true },
   {hintName: 'Tipos despesas', field: 'name', header: 'Nome', showCol: true },
-  {hintName: 'Descrição do tipos despesas', field: 'cor', header: 'Cor', showCol: true },
+  {hintName: 'Cor da despesa', field: 'cor', header: 'Cor', showCol: true },
   {hintName: 'Operações Disponíveis', field: 'operation', header: 'OPERAÇÕES', showCol: true }
 ];
