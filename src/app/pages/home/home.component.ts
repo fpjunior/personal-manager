98c59dcbc5e59e76278267f3ac39dd8141626fb6
ivsonramos <ivson.ramos@gmail.com>
2022-08-19 20:57:28 -0300

    :rocket:Adiciona dropdown para seleção do mês atual

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,13 +9,41 @@ export class HomeComponent implements OnInit {
 
   date1: Date;
   es: any;
-  dateValue: Date;
+  //dateValue: Date;
+  optionsMonth = [
+    { value: '1', label: 'Janeiro' },
+    { value: '2', label: 'Fevereiro' },
+    { value: '3', label: 'Março' },
+    { value: '4', label: 'Abril' },
+    { value: '5', label: 'Maio' },
+    { value: '6', label: 'Junho' },
+    { value: '7', label: 'Julho' },
+    { value: '8', label: 'Agosto' },
+    { value: '9', label: 'Setembro' },
+    { value: '10', label: 'Outubro' },
+    { value: '11', label: 'Novembro' },
+    { value: '12', label: 'Dezembro' }
+  ]
+
+  mesAtual;
+  months;
+  dateValue = new Date();
+
+
 
   constructor() {
-
   }
 
   ngOnInit() {
+    this.getActualMonth();
 
+  }
+
+  getActualMonth() {
+    this.months = this.optionsMonth.filter(month => {
+      return month.value == (this.dateValue.getMonth() + 1).toString();
+    });
+    this.mesAtual = this.months[0].label; console.log(this.mesAtual); return this.dateValue.getMonth() + 1;
+  }
 }
-}
+
